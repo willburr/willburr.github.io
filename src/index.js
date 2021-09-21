49e1747ff696b2c043a2e33ba9b609bb7c09cb3e
Will Burr <williamburr98@gmail.com>
2021-09-21 14:16:42 +0100

    Add a timer

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 import _ from "lodash";
 
-const createNewElement = () => {
-    const element = document.createElement("div");
-    element.innerText = _.join(['Hello', 'there'], ' ');
+var timer; 
+var time = 0;
 
-    return element;
+const timeText = document.getElementById("time-txt");
+
+const tick = () => {
+    time += 1;
+    const centiseconds = (time % 100).toString().padStart(2, "0");
+    const hours = Math.floor(time / (60 * 60 * 100)).toString().padStart(2, "0");
+    const minutes = Math.floor(time / (60 * 100)).toString().padStart(2, "0");
+    const seconds = Math.floor(time / 100).toString().padStart(2, "0");
+
+    timeText.innerText = `${hours}:${minutes}:${seconds}.${centiseconds}`;
 };
 
-document.body.appendChild(createNewElement());
+document.getElementById("start-btn").addEventListener('click', () => {
+    // Start timer
+    if (!timer) {
+        timer = setInterval(tick, 10);
+    }
+});
+
+document.getElementById("end-btn").addEventListener('click', () => {
+    // Stop if possible
+    if (timer) {
+        clearInterval(timer);
+        timer = null;
+    }
+});
